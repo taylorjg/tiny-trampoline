2c1f2c4a7abb4fa70050f6c95ab58ec7edfd71cc
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-06-11 21:55:59 +0100

    Add T.recurse and T.done

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-const T = fn => (...args) => {
-  let returnValue = fn(...args)
-  for (; ;) {
-    if (typeof returnValue === 'function') {
-      returnValue = returnValue()
-    } else {
-      return returnValue
+const RECURSE = Symbol('RECURSE')
+const DONE = Symbol('DONE')
+
+const T = {
+  recurse: thunk => ({
+    type: RECURSE,
+    thunk
+  }),
+  done: result => ({
+    type: DONE,
+    result
+  }),
+  trampoline: fn => (...args) => {
+    let returnValue = fn(...args)
+    for (; ;) {
+      switch (returnValue.type) {
+        case RECURSE:
+          returnValue = returnValue.thunk()
+          break
+        case DONE:
+          return returnValue.result
+        default:
+          throw new Error(`Unexpected returnValue.type ${returnValue.type}`)
+      }
     }
   }
 }
 
-const factorial = T(function _factorial(n, acc = 1) {
+const factorial = T.trampoline(function _factorial(n, acc = 1) {
   return n > 1
-    ? () => _factorial(n - 1, acc * n)
-    : acc
+    ? T.recurse(() => _factorial(n - 1, acc * n))
+    : T.done(acc)
 })
 
 const ns = Array.from(Array(20).keys())
